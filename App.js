f35cd9e79f266bd2e071c63476c0e81249b0a930
AmirHossein <Amirhosseinaghaee22@gmail.com>
2023-05-24 17:29:50 +0330

    animated splash

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
+import {useEffect, useState} from "react";
 import {useFonts} from 'expo-font';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import AnimatedSplash from "react-native-animated-splash-screen";
 import theme from "./theme/theme";
-import eifleConfigHeader from "./components/eifelHeaderConfig";
 import Login from "./screen/Login";
 import Profile from "./screen/Profile";
-import test from "./screen/CustomDrawer";
+
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+    const [Loading , setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(()=>{
+            setLoading(false)
+            console.log("a ima here ...." + Loading)
+        } , 2000)
+    },[])
 
     const [loadedFonts] = useFonts({
         ExtraLight: require('./assets/fonts/Vazirmatn-RD-FD-ExtraLight.ttf'),
@@ -24,24 +32,40 @@ export default function App() {
         SemiBold: require('./assets/fonts/Vazirmatn-RD-FD-SemiBold.ttf'),
         Thin: require('./assets/fonts/Vazirmatn-RD-FD-Thin.ttf'),
     });
+
+
+
     //
     if (!loadedFonts) {
         return false;
     }
 
+
+
     return (
+        <AnimatedSplash
+            translucent={true}
+            isLoaded={false}
+            logoImage={require("./assets/splash.png")}
+            backgroundColor={theme.colors.primary}
+            logoHeight={200}
+            logoWidth={200}
+        >
         <PaperProvider theme={theme}>
-            <NavigationContainer>
-                <Stack.Navigator
-                    screenOptions ={{
-                        // gestureEnable: true,
-                        // gestureDirection : "horizontal"
-                    }}
-                >
-                    <Stack.Screen name="Home" component={Login} options={{headerShown:false}}/>
-                    <Stack.Screen name="Profile" component={Profile} options={{headerShown:false}}/>
-                </Stack.Navigator>
-            </NavigationContainer>
+
+                <NavigationContainer>
+                    <Stack.Navigator
+                        screenOptions ={{
+                            // gestureEnable: true,
+                            // gestureDirection : "horizontal"
+                        }}
+                    >
+                        <Stack.Screen name="Home" component={Login} options={{headerShown:false}}/>
+                        <Stack.Screen name="Profile" component={Profile} options={{headerShown:false}}/>
+                    </Stack.Navigator>
+                </NavigationContainer>
+
         </PaperProvider>
+    </AnimatedSplash>
     );
 }
